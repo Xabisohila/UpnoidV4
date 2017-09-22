d50b06a8b16c8bb8ec0301438998172550d2d362
Xabisohila <okxabisohila@gmail.com>
2017-09-22 13:47:02 +0200

    Add project files.

---
+++ UpnoidV4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UpnoidV4.MvcApplication" Language="C#" %>
---
+++ UpnoidV4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UpnoidV4.Startup))]
+namespace UpnoidV4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
